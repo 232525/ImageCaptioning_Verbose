7669574734a470d4f7eba1687ef27b2ffb173cc6
232525 <wangyiyu18@mails.ucas.ac.cn>
2024-06-03 12:02:40 +0800

    update

--- experiments/experiments_UpDown/UpDown_SCST/train.sh
+++ experiments/experiments_UpDown/UpDown_SCST/train.sh
@@ -1,3 +1,3 @@
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --master_port=3140 --nproc_per_node=4 main_multi_gpu.py --folder ./experiments/experiments_UpDown/UpDown_SCST/ --resume *
 
-CUDA_VISIBLE_DEVICES=0 python main.py --folder ./experiments/experiments_UpDown/UpDown_SCST/ --resume *
+CUDA_VISIBLE_DEVICES=1 python main.py --folder ./experiments/experiments_UpDown/UpDown_SCST/ --resume 14
